274ca9807151c235e83b797a1b5dfe44cc52cc94
pankajmanral <pankajmanral0208@gmail.com>
2024-09-10 14:15:34 +0530

    commited

--- main/karma/settings.py
+++ main/karma/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
+    # 'product.apps.ProductConfig',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
